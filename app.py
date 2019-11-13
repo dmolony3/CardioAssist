c1e00bdbabfe896389320c7be6a988c583f7e9ad
dmolony3 <dmolony@emory.edu>
2019-11-13 08:40:29 -0500

    added variable number of responses

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from pytorch_transformers import GPT2Config, GPT2Tokenizer, GPT2LMHeadModel
 import torch
-
+import random
+from waitress import serve
 
 app = Flask(__name__)
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model = GPT2LMHeadModel.from_pretrained('gpt2')
+"""
+config = GPT2Config.from_pretrained('gpt2-medium')
+config.output_hidden_states=True
+config.output_attentions=True
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
+model = GPT2LMHeadModel.from_pretrained('gpt2-medium', config=config)
 model.eval()
+"""
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
 
 # load model only once
 def load_model():
@@ -25,22 +32,66 @@ def decode_text(tokens_tensor):
 	text = tokenizer.decode(tokens_tensor)
 	return text
 
-def predict(text):
-	tokens_tensor = encode_text(text)
-	with torch.no_grad():
-		outputs = model(tokens_tensor)
-		predictions = outputs[0]
-		predictions = torch.argmax(predictions[0, -1, :]).item()
-	text = decode_text(predictions)
-	return text
-	
+class Model():
+	def __init__(self):
+		config = GPT2Config.from_pretrained('gpt2-medium')
+		config.output_hidden_states=True
+		config.output_attentions=Truetokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
+		self.model = GPT2LMHeadModel.from_pretrained('gpt2-medium', config=config)
+		self.model.eval()
+		self.context = ''
+		
+	def predict(self, context, past, temperature, top_k, idx=None):
+		self.context = context
+		self.tokens_tensor = encode_text(self.context)
+		with torch.no_grad():
+			self.outputs = self.model(self.tokens_tensor, past)
+			logits = self.outputs[0]/temperature # higher temperature will spread out the probability i.e. generate more diverse output
+			past = self.outputs[1]
+			predictions = torch.nn.functional.softmax(logits[0, -1, :], dim=-1)
+			if top_k == 1:
+				prediction_idx = torch.argmax(predictions).item()
+			else:
+				_, prediction_top_k_idx = torch.topk(predictions, top_k)
+				if idx is None:
+					predictions = predictions[prediction_top_k_idx]
+					prediction_idx = prediction_top_k_idx[torch.multinomial(predictions, 1)].item()
+				else:
+					prediction_idx = prediction_top_k_idx[idx].item()
+				#idx = random.randint(0, len(predictions)-1)
+				#prediction_idx = predictions[idx].item()
+
+		text = decode_text(prediction_idx)
+		return text, past	
+		
+	def get_attention(self, layer, head, text):
+		batch_i = 0
+		"""
+		text_tokens = tokenizer.tokenize(text)
+		context_tokens = tokenizer.tokenize(self.context)
+		token_idx = [i for i in range(len(context_tokens)) if context_tokens[i] in text_tokens]
+		attention = self.outputs[-1][layer][batch_i][head][token_idx][:, token_idx]
+		tokens = [context_tokens[idx] for idx in token_idx]
+		"""
+		self.tokens_tensor = encode_text(text)
+		with torch.no_grad():
+			self.outputs = self.model(self.tokens_tensor)
+		attention = self.outputs[-1][layer][batch_i][head]
+		tokens = tokenizer.tokenize(text)
+
+		return attention, tokens		
+		
+model = Model()
+
 def get_model():
 	text = 'Default text'
 	return text
 	
 @app.route('/')
 def index():
-	return render_template('index.html', newText="")
+	heads = model.model.config.n_head
+	layers = model.model.config.n_layer
+	return render_template('index.html', newText="", numHeads=heads, numLayers=layers)
 
 def display(text):
    return render_template('index.html', newText = text)
@@ -58,21 +109,58 @@ def process():
 
 @app.route('/process1', methods=['POST', 'GET'])
 def process1():
+	"""Returns responses from model based on initial_context
+	Receives temperature, top_k, num_responses and initial_context from app.
+	If num_responses > 1 then the top-k next wordsare used as inital seeds for the next world
+	Returns json object for each response
+	"""
 	if request.method == 'POST':
 		result = request.get_json()
-		#result = request.args.get('text')
-		#text = get_model()
-		text = predict(result['text'])
-		print(result['text'])
-		print(text)
-		text = {'text': result['text'] + text}
+		temperature = float(result['temperature'])
+		top_k = int(result['top-k'])
+		num_responses = int(result['num-responses'])
+		initial_context = result['text'].strip()
+		text = []
+		for i in range(num_responses):
+			print('Response {}'.format(i))
+			past = None
+			if num_responses > 1:
+				next_word, past = model.predict(initial_context, past, temperature, num_responses, i)
+			else:
+				next_word, past = model.predict(initial_context, past, temperature, top_k, i)
+				
+			context = initial_context + next_word
+			print(context)
+			generated_length = 1
+			while next_word != '.' and next_word != tokenizer.eos_token and generated_length < 25:
+				next_word, past = model.predict(next_word, past, temperature, top_k)
+				context = context + next_word
+				print(next_word)
+				print(context)
+				generated_length += 1
+			new_text = context.replace(initial_context, ' ')
+			print(new_text)
+			text.append({'text': new_text})
 		# to send multiple strings to client wrap in list e.g. text= [{'text':'hello'},{'text':'how'}]
 		return jsonify(text)
 	else:
 		print('did not work')
 	return redirect('/')
 
-	
+@app.route('/plot_attention', methods=['GET', 'POST'])
+def plot_attention():
+	if request.method == 'POST':
+		params = request.get_json()
+		layer = int(params['layer'])
+		head = int(params['head'])
+		text = params['text']
+		attention, text_tokens = model.get_attention(layer, head, text)
+		text_tokens = [token.replace('Ġ', '') for token in text_tokens]
+		print('Returning attention for layer {} head {}'.format(layer, head))
+		attention = [{'text1': text_tokens[i], 'text2': text_tokens[j], 'attentionValue': attention[i][j].item()} for i in range(attention.shape[0]) for j in range(attention.shape[1])]
+	return jsonify(attention)
+
 if __name__ == 'main':
-	load_model()
+	#load_model()
 	app.run()
+	#serve(app, host='0.0.0.0', port=8000)
--- static/main.js
+++ static/main.js
@@ -1,28 +1,269 @@
-/*
-$('#textBox').keypress(function(event){
-	event.preventDefault();
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if(keycode == '13'){
-        alert('You pressed a "enter" key in textbox'); 
-    }
-});
-$('#textBox').live('keypress', function(e) {
-    if (e.KeyCode == 70) {
-        e.preventDefault();
-        alert("or Shift was pressed");
-    }
-	alert('help');
+var modal = document.getElementById("myModal");
+var numberOfSuggestions = $('tr').length;
+var tableOpened = 0;
+var rowIndex = 0;
+var highlightedText
+
+
+var margin = {top: 50, right: 50, bottom: 50, left: 50},
+  width = 300 - margin.left - margin.right,
+  height = 300 - margin.top - margin.bottom;
+
+var myColor = d3.scaleSequential()
+    .interpolator(d3.interpolateMagma)
+    .domain([0,1])
+
+function createHeatmap(data) {
+// append the svg object to the body of the page
+ 
+//labels 
+var tickNames = [...new Set(data.map(a => a.text1))];
+// Build X scales and axis: // the domain gets  mapped to the range
+var x = d3.scaleBand()
+  .range([ 0, width ])
+  .domain(tickNames)
+  .padding(0.01);
+var xAxis = d3.axisBottom()
+  .scale(x);
+  
+svg.append("g").attr('class','x axis')
+  .attr("transform", "translate(0," + height + ")")
+  .call(xAxis)
+  .selectAll("text")	
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-90)");
+  
+// Build Y scales and axis:
+var y = d3.scaleBand()
+  .range([ height, 0 ])
+  .domain(tickNames)
+  .padding(0.01);
+var yAxis = d3.axisLeft()
+  .scale(y);
+  
+svg.append("g").attr('class','y axis')
+  .call(yAxis);
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<span style='color:red'>" + d.attentionValue.toFixed(2) + "</span>";
+  })
+svg.call(tip);
+  
+  var myColor = d3.scaleSequential()
+    .interpolator(d3.interpolateMagma)
+    .domain([0,1])
+
+  svg.selectAll()
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", function(d) { return x(d.text1) })
+      .attr("y", function(d) { return y(d.text2) })
+      .attr("width", x.bandwidth() )
+      .attr("height", y.bandwidth() )
+      .style("fill", function(d) { return myColor(d.attentionValue)} )
+      .on("mouseover", tip.show)
+	  .on("mouseout", tip.hide)
+}
+
+var svg = d3.select(".attention")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+function updateAxis(text) {
+	x.domain(text);
+	//xaxis.scale = x;
+	svg.select('.x.axis')
+	.transition().duration(1000)
+	.call(xAxis);
+	y.domain(text);
+	svg.select('.y.axis')
+	.transition().duration(1000)
+	.call(yAxis)
+}
+
+var layerMenu = d3.select("#layerDropdown");
+var numLayers = $("#numLayers").val();
+layers = Array.from({ length: parseInt(numLayers) }, (_, k) => `Layer${k+1}`);
+
+layerMenu
+  .append("select")
+  .attr("id", "layerMenu")
+  .selectAll("option")
+	.data(layers)
+	.enter()
+	.append("option")
+	.attr("value", function(d, i) { return i; })
+	.text(function(d) { return d; });
+
+var headMenu = d3.select("#headDropdown");
+var numHeads = $("#numHeads").val();
+heads = Array.from({ length: parseInt(numHeads) }, (_, k) => `Head${k+1}`);
+
+headMenu
+  .append("select")
+  .attr("id", "headMenu")
+  .selectAll("option")
+	.data(heads)
+	.enter()
+	.append("option")
+	.attr("value", function(d, i) { return i; })
+	.text(function(d) { return d; });
+
+$('#layerMenu').change(function() {
+	var selectedHead = parseInt($('#headMenu').val())
+	var selectedLayer = parseInt($(this).val())
+    //currentHeadIndex = +selectedHead;
+	alert(highlightedText)
+	$.ajax({
+	type: 'POST',
+	url: 'plot_attention',
+	data: JSON.stringify({"text": highlightedText, "head": selectedHead, "layer": selectedLayer}),
+	dataType: 'json',
+	contentType: 'application/json; charset=utf-8',
+	success: function(data) {
+		var updateHeatmap = function(data) {
+		    var heatmap = svg.selectAll('rect')
+			  .data(data)
+			  .transition()
+			  .duration(500)
+			  .style("fill", function(d) { return myColor(d.attentionValue)} )
+	}
+		updateHeatmap(data);		  
+	}
+	});
 });
 
-$(document).on('keypress', '#textBox', function(e){
-	var keyCode = e.KeyCode.val();
-	if (e.KeyCode == 70) {
-		e.preventDefault();
-		alert(e.which + " or Shift was pressed");
+$('#headMenu').change(function() {
+	var selectedHead = parseInt($(this).val())
+	var selectedLayer = parseInt($('#layerMenu').val())
+    //currentHeadIndex = +selectedHead;
+	alert(highlightedText)
+	$.ajax({
+	type: 'POST',
+	url: 'plot_attention',
+	data: JSON.stringify({"text": highlightedText, "head": selectedHead, "layer": selectedLayer}),
+	dataType: 'json',
+	contentType: 'application/json; charset=utf-8',
+	success: function(data) {
+		var updateHeatmap = function(data) {
+		// filter data to return object of location of interest
+		// update the data and redraw heatmap
+		/*
+			var heatmap = svg.selectAll(".attentionValue")
+			  .data(data)
+			  .transition()
+			  .duration(500)
+			  .style("fill", function(d) { return myColor(d.attentionValue)})
+		  }*/
+		    var heatmap = svg.selectAll('rect')
+			  .data(data)
+			  .transition()
+			  .duration(500)
+			  .style("fill", function(d) { return myColor(d.attentionValue)} )
+			//heatmap.exit().remove();
+			console.log(data[1]['attentionValue'])
+			//var heatmap = svg.selectAll('rect')
+			//  .style("fill", 'red')
 	}
-	alert('help');
+		updateHeatmap(data);		  
+	}
+	});
 });
-*/
+
+$("#addBtn").click(function() {
+	selectedHead = parseInt($('#headMenu').val())
+	selectedLayer = parseInt($('#layerMenu').val())
+	$("#tooltip").hide();
+  	$.ajax({type: 'POST',
+		  url: 'plot_attention',
+		  data: JSON.stringify({"text": selection, "head": selectedHead, "layer": selectedLayer}),
+		  dataType: 'json',
+		  contentType: 'application/json; charset=utf-8',
+          success: function (data) {
+			var numberOfTokens = data.length;
+			svg.selectAll("*").remove();
+			createHeatmap(data);
+		  }
+		  });
+});
+		
+function placeTooltip(x_pos, y_pos) {
+  $("#tooltip").css({
+    top: y_pos + 'px',
+    left: x_pos + 'px',
+    position: 'absolute'
+  });
+}
+	  
+$(document).mouseup(function(e) {  
+   // get selected text
+  // http://stackoverflow.com/questions/5379120/get-the-highlighted-selected-text
+  selection = window.getSelection().toString();
+  if (selection.length > 0) {
+  if (e.target.id === 'textBox') {
+  $('#selTxt').val(selection.toString());
+	highlightedText = selection;
+    var x = e.pageX;
+    var y = e.pageY;  
+    placeTooltip(x, y);
+    $("#tooltip").show();
+  }
+  }
+  else {
+	$("#tooltip").hide() 
+ }
+  });
+  
+$(document).keydown(function (e) {
+	if (tableOpened === 1) {
+    switch(e.which) 
+    {
+        case 38: //up
+            --rowIndex
+            if (rowIndex < 0) {
+          		  rowIndex = $('tr').length -1
+            }
+            highlight(rowIndex);
+            break;
+        case 40:  //down
+            ++rowIndex
+        		if (rowIndex+1 > $('tr').length) {
+           		 rowIndex = 0;
+            }
+            highlight(rowIndex);
+            break;
+      	case 13: //return
+            e.preventDefault()
+            selectedText = $('tr')[rowIndex].innerText;
+            currentText = $('#textBox').text();
+            $('#textBox').text(currentText + selectedText);
+            tableOpened = 0;
+            rowIndex = 0;
+            modal.style.display = "none";
+            document.getElementById("textBox").focus();
+            placeCaretAtEnd(document.getElementById("textBox"));
+            break;
+      	case 27: //escape
+            tableOpened = 0;
+            rowIndex = 0;
+            modal.style.display = "none";
+            document.getElementById("textBox").focus();
+            placeCaretAtEnd(document.getElementById("textBox"));
+            break;
+    }
+    }
+ });
+ 
 (function() {
 
 var tabKeyPressed = false;
@@ -36,44 +277,136 @@ $("#textBox").keydown(function(e) {
 });
 
 $("#textBox").keyup(function(e) {
-   var initialText = {"text": $(this).val()};
-   if (tabKeyPressed) {
+    //var initialText = {"text": $(this).val()};
+    var initialText = $('.initialText').text();
+    var temperature = $('#slider-temp').val();
+    var top_k = $('#slider-top-k').val();
+    var num_responses = $('#num-responses').val();
+	var modal = document.getElementById("myModal");
+    if (tabKeyPressed) {
+		$('#textBox').addClass('spinner');
+		var initialText = $('.textarea').text();
 	    alert(initialText);
 		$.ajax({
 		  type: 'POST',
 		  url: 'process1',
-		  data: JSON.stringify(initialText),
+		  data: JSON.stringify({"text": initialText, "temperature": temperature, "top-k": top_k, "num-responses": num_responses}),
 		  dataType: 'json',
 		  contentType: 'application/json; charset=utf-8',
           success: function (data) 
 		{
-            alert(JSON.stringify(data));
-			$("#textBox").val(data['text']);
+			$('#textBox').removeClass('spinner');
+			var currentTableLength = document.getElementById('table-data').getElementsByTagName('tr').length;
+			while (currentTableLength != num_responses) {
+				if (currentTableLength > num_responses) {
+					document.getElementById("table-data").deleteRow(currentTableLength - 1);					
+				}
+				else if (currentTableLength < num_responses){
+					$('#table-data').append("<tr><td>NewText</td></tr>");
+				}
+				currentTableLength = document.getElementById('table-data').getElementsByTagName('tr').length;
+			}
+			for (row = 0; row < num_responses; row++) 
+			{
+				$('tr')[row].innerText = data[row]['text']
+			}
+			var positionTop = document.getElementById("textBox").offsetTop;
+			var positionLeft = document.getElementById("textBox").offsetLeft;
+			var caretPos = getCaretPosition(positionLeft, positionTop);
+			modal.style.display = "block";
+			$('.modal-content').offset({ top: caretPos.y, left: caretPos.x + 5});
+			highlight(0);
+			tableOpened = 1;
+     		//$('.textarea').text(initialText + data['text']);
+			//$('.initialText').text(initialText);
+			//$('.newText').text(data['text']);
+			//placeCaretAtEnd( document.querySelector('.textarea') );
         }
-	  });
-      //$(this).val("TAB"); // Do stuff for TAB
-      e.preventDefault();
-	  //window.open("http://www.google.com/");
-      return;
-   }
+	    });
+		
+        e.preventDefault();
+        return;
+    }
 
-   //Do other stuff when not TAB
 });
 })();
 
-/*
-$("#textBox").keyup(function(e) {
-   if (tabKeyPressed) {
-      $(this).val("TAB"); // Do stuff for TAB
-      e.preventDefault();
-	  window.open("http://www.google.com/");
-      return;
-   }
-
-   //Do other stuff when not TAB
-});
-
-*/
+function charcountupdate(str) {
+	var lng = str.length;
+	document.getElementById("charcount").innerHTML = lng + ' characters';
+}
 
 
+function displayModal(){
+  modal.style.display = "block";
+}
 
+function highlight(tableIndex) {
+    // Just a simple check. If .highlight has reached the last, start again
+    if( (tableIndex+1) > $('#table-data tr').length )
+        tableIndex = 0;
+
+    // Element exists?
+    if($('#table-data tr:eq('+tableIndex+')').length > 0)
+    {
+        // Remove other highlights
+        $('#table-data tr').removeClass('highlight');
+
+        // Highlight your target
+        $('#table-data tr:eq('+tableIndex+')').addClass('highlight');
+    }
+}
+
+function placeCaretAtEnd(el) {
+    el.focus();
+    if (typeof window.getSelection != "undefined"
+            && typeof document.createRange != "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (typeof document.body.createTextRange != "undefined") {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
+    }
+}
+
+function getCaretPosition(x, y) {
+
+    var sel = window.getSelection();
+    if(sel.rangeCount) {
+        var range = sel.getRangeAt(0).cloneRange();
+        if(range.getClientRects()) {
+        range.collapse(true);
+        var rect = range.getClientRects()[0];
+        if(rect) {
+            y = rect.top;
+            x = rect.left;
+        }
+        }
+    }
+    return {
+        x: x,
+        y: y
+    };
+}
+
+function tweetIt () {
+	var phrase = document.getElementById('textBox').innerText;
+	var tweetUrl = 'https://twitter.com/share?hashtags=CardioAssist' +
+	'&text=' +
+    encodeURIComponent(phrase) +
+    '.' +
+    'AI text generated by ' +
+    '&url=' +
+	'http://CardioAssist.com/' +
+	'&via=DavidSMolony';
+    
+	window.open(tweetUrl);
+}
+
+$('[data-toggle="popover"]').popover();
